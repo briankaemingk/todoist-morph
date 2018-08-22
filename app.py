5a3ca419f4685f2cc5ed5446d428dabee1809b1b
Brian Kaemingk <brian.e.k@gmail.com>
2018-08-21 23:42:43 -0700

    Initial draft of recurrance snooze branch

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import morph
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def main():
+def index():
     morph.main()
     return 'Completed'
 
+@app.route('/complete')
+def complete():
+    task_url = str(request.data)
+    morph.task_complete(task_url)
+    return 'Completed task complete actions.'
 
 if __name__ == '__main__':
     app.run()
--- morph.py
+++ morph.py
@@ -1,14 +1,18 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 from pathlib import Path  # python3 only
+
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 import os
 from datetime import datetime
 from dateutil.parser import parse
 import pytz
+import re
 
 from todoist.api import TodoistAPI
+
 
 def get_token():
     token = os.getenv('TODOIST_APIKEY')
@@ -21,16 +25,22 @@ def update_overdue_to_all_day(due_date, now, task):
     if due_date <= now and due_date.date() == now.date():
         print("Identified newly due task: {0}".format(task["content"]))
         new_due_date = datetime(year=now.year,
-                            month=now.month,
-                            day=now.day,
-                            hour=23,
-                            minute=59,
-                            second=59).astimezone(pytz.utc)
+                                month=now.month,
+                                day=now.day,
+                                hour=23,
+                                minute=59,
+                                second=59).astimezone(pytz.utc)
         return new_due_date
+
+def get_user_timezone(api):
+    return pytz.timezone(api.state["user"]["tz_info"]["timezone"])
+
+def is_recurrance_snooze(task_content):
+    return re.search(r'\s{(\d+:\d+)}', task_content)
 
 def update_overdue_tasks(api):
     # Get user's timezone
-    user_timezone = pytz.timezone(api.state["user"]["tz_info"]["timezone"])
+    user_timezone = get_user_timezone(api)
 
     # Get current time in user's timezone
     now = datetime.now(tz=user_timezone)
@@ -48,6 +58,14 @@ def update_overdue_tasks(api):
                 task.update(due_date_utc=new_due_date)
                 print("Updating task <{0}> to due date: {1}".format(task["content"], new_due_date))
 
+def update_recurrance_date(api, task_url):
+    user_timezone = get_user_timezone(api)
+
+    tasks = api.state['items']
+    for task in tasks:
+        if task["due_date_utc"] and is_recurrance_snooze(task["content"]):
+            due_time = is_recurrance_snooze(task["content"]).group(1)
+
 def main():
     API_TOKEN = get_token()
     if not API_TOKEN:
@@ -56,4 +74,15 @@ def main():
     api = TodoistAPI(API_TOKEN)
     api.sync()
     update_overdue_tasks(api)
+    api.commit()
+
+
+def task_complete(task_url):
+    API_TOKEN = get_token()
+    if not API_TOKEN:
+        logging.warn('Please set the API token in environment variable.')
+        exit()
+    api = TodoistAPI(API_TOKEN)
+    api.sync()
+    update_recurrance_date(api, task_url)
     api.commit()
