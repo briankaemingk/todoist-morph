956a89a0fd193d18190820d3e0948edf2af0d5e2
Brian Kaemingk <brian.e.k@gmail.com>
2018-08-22 22:56:35 -0700

    Working version with task completion trigger

--- morph.py
+++ morph.py
@@ -18,37 +18,44 @@ def get_token():
     token = os.getenv('TODOIST_APIKEY')
     return token
 
+
 def is_overdue(due_date, now):
-    if due_date <= now and due_date.date() == now.date() : return 1
+    if due_date <= now and due_date.date() == now.date(): return 1
+
 
 # If due in the past and it's due today,
 # update to end of today (default for all day tasks)
 def update_to_all_day(now, task):
-        new_due_date = datetime(year=now.year,
-                                month=now.month,
-                                day=now.day,
-                                hour=23,
-                                minute=59,
-                                second=59).astimezone(pytz.utc)
-        return new_due_date
+    new_due_date = datetime(year=now.year,
+                            month=now.month,
+                            day=now.day,
+                            hour=23,
+                            minute=59,
+                            second=59).astimezone(pytz.utc)
+    return new_due_date
+
 
 # Get user's timezone
 def get_user_timezone(api):
     return pytz.timezone(api.state["user"]["tz_info"]["timezone"])
+
 
 # Get current time in user's timezone
 def get_now_user_timezone(api):
     user_timezone = get_user_timezone(api)
     return datetime.now(tz=user_timezone)
 
+
 def is_recurrance_snooze(task_content):
     return re.search(r'\s{(\d+:\d+)}', task_content)
 
+
 def parse_task_id(task_url):
-    #URL is in format: https://todoist.com/showTask?id=2690174754
+    # URL is in format: https://todoist.com/showTask?id=2690174754
     task_match = re.search('https:\/\/todoist.com\/showTask\?id=([0-9]+)', task_url)
     task_id = task_match.group(1)
     return task_id
+
 
 def update_overdue_tasks(api):
     user_timezone = get_user_timezone(api)
@@ -66,13 +73,35 @@ def update_overdue_tasks(api):
                 task.update(due_date_utc=new_due_date)
                 print("Updating task <{0}> to due date: {1}".format(task["content"], new_due_date))
 
+
+def convert_time_str_datetime(time_str, user_timezone):
+    return datetime.strptime(time_str, '%a %d %b %Y %H:%M:%S %z').astimezone(user_timezone)
+
+
+def convert_datetime_str(date):
+    return date.strftime('%Y-%m-%dT%H:%M')
+
+
+def replace_due_date_time(new_due_time, due_date_utc, user_timezone):
+    due_date_localtz_date = convert_time_str_datetime(due_date_utc, user_timezone)
+    new_due_time_split = new_due_time.split(":")
+    new_due_date_localtz_date = due_date_localtz_date.replace(hour=int(new_due_time_split[0]), minute=int(new_due_time_split[1]))
+    new_due_date_utc_date = new_due_date_localtz_date.astimezone(pytz.utc)
+    return new_due_date_utc_date
+
+
 def update_recurrance_date(api, task_url):
     user_timezone = get_user_timezone(api)
     task_id = parse_task_id(task_url)
     task = api.items.get_by_id(int(task_id))
     if task["due_date_utc"] and is_recurrance_snooze(task["content"]):
-        due_time = is_recurrance_snooze(task["content"]).group(1)
-        print(due_time)
+        new_due_time = is_recurrance_snooze(task["content"]).group(1)
+        new_due_date_utc = replace_due_date_time(new_due_time, task["due_date_utc"], user_timezone)
+        new_due_date_utc_str = convert_datetime_str(new_due_date_utc)
+        task.update(due_date_utc=new_due_date_utc_str)
+        task.update(content= re.sub(is_recurrance_snooze(task["content"]).group(0), '', task["content"]))
+        api.commit()
+
 
 def main():
     API_TOKEN = get_token()
