d5bb22eaa4ffe1490c8200bb37850f5bbe6d828e
Brian Kaemingk <brian.e.k@gmail.com>
2018-08-28 17:36:46 -0700

    Requirements with dependencies

--- morph.py
+++ morph.py
@@ -1,10 +1,3 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-from pathlib import Path  # python3 only
-
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
 import os
 from datetime import datetime
 from dateutil.parser import parse
